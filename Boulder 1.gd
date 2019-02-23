207b9f02186cc759d6728dfa8604cd76a8b37f4f
Ola Theander <ola.theander@myola.se>
2019-02-23 17:34:39 +0100

    Implemented split into smaller blocks on hit.

--- Boulder 1.gd
+++ Boulder 1.gd
@@ -1,26 +1,17 @@
 extends KinematicBody2D
 
-const BOULDER = preload("res://Boulder 1.tscn")
-
 signal boulder_removed
+signal boulder_hit
 
-# export (float) var rotation_speed = 0.1
 export (int) var speed = 100
 export (Vector2) var velocity = Vector2()
-export (int) var spriteIndex = 0
 
 func _ready():
-    # Called when the node is added to the scene for the first time.
-    # Initialization here
-    print(spriteIndex)
-    $Sprite.frame = spriteIndex
+    pass
 
 func _physics_process(delta):
-    # velocity = Vector2(speed, 0).rotated(0) * delta
-    var collision = move_and_collide(velocity * delta)
+    move_and_collide(velocity * delta)
     _remove_when_offscreen()
-    if collision:
-        velocity = velocity.bounce(collision.normal)
         
 func _remove_when_offscreen():
     var screensize = get_viewport_rect().size
@@ -36,18 +27,5 @@ func _remove_rock():
     emit_signal("boulder_removed")
             
 func on_bullet_hit(bullet):
+    emit_signal("boulder_hit", self, 1)
     _remove_rock()
-    print(str("Hit: ", spriteIndex))
-    if spriteIndex < 3:
-        # Spawn small blocks
-        var boulder = BOULDER.instance()
-        get_parent().add_child(boulder)
-        # Set the boulder's position to a random location.
-        boulder.position = position
-        # Add some randomness to the direction.
-        # direction += rand_range(-PI / 4, PI / 4)
-        # boulder.rotation = direction
-        # boulder.velocity = Vector2(boulder.speed, 0).rotated(direction)
-        boulder.spriteIndex = spriteIndex + 1
-        #boulder.velocity = velocity.rotated(rand_range(-PI / 4, PI / 4))
-        boulder.velocity = velocity
--- Main.gd
+++ Main.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 const MAX_SPAWNED = 5
-const BOULDER = preload("res://Boulder 1.tscn")
+const BOULDER1 = preload("res://Boulder 1.tscn")
+const BOULDER2 = preload("res://Boulder 2.tscn")
+const BOULDER3 = preload("res://Boulder 3.tscn")
+const BOULDER4 = preload("res://Boulder 4.tscn")
+
 var score = 0
 var currentSpawned = 0
 
@@ -35,8 +39,9 @@ func _on_BoulderTimer_timeout():
         # Choose a random location on Path2D.
         $BoulderPath/BoulderSpawnLocation.set_offset(randi())
         # Create a Boulder instance and add it to the scene.
-        var boulder = BOULDER.instance()
+        var boulder = BOULDER1.instance()
         boulder.connect("boulder_removed", self, "_on_Boulder_boulder_removed")
+        boulder.connect("boulder_hit", self, "_on_Boulder_boulder_hit")
         add_child(boulder)
         # Set the boulder's position to a random location.
         boulder.position = $BoulderPath/BoulderSpawnLocation.position
@@ -51,4 +56,32 @@ func _on_BoulderTimer_timeout():
 
 func _on_Boulder_boulder_removed():
     currentSpawned -= 1
-    
+  
+func _on_Boulder_boulder_hit(boulder, type):
+    var velocity = boulder.velocity
+    var newBoulder
+    for x in [-1, 0, 1]:
+        match type:
+            1:
+                newBoulder = BOULDER2.instance()
+                newBoulder.position = boulder.position
+                newBoulder.velocity = boulder.velocity.rotated(x * PI/4)
+                newBoulder.connect("boulder_hit", self, "_on_Boulder_boulder_hit")
+                add_child(newBoulder)
+            2:
+                newBoulder = BOULDER3.instance()
+                newBoulder.position = boulder.position
+                newBoulder.velocity = boulder.velocity.rotated(x * PI/4)
+                newBoulder.connect("boulder_hit", self, "_on_Boulder_boulder_hit")
+                add_child(newBoulder)
+            3:
+                newBoulder = BOULDER4.instance()
+                newBoulder.position = boulder.position
+                newBoulder.velocity = boulder.velocity.rotated(x * PI/4)
+                newBoulder.connect("boulder_hit", self, "_on_Boulder_boulder_hit")
+                add_child(newBoulder)
+            4:
+                pass
+             
+
+
